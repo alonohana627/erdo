efe420c513620d61dc9ac9692fe11d7e2e69d12f
Alon Ohana <alon4dota2@gmail.com>
2023-08-05 01:24:55 +0300

    Added basic type traits and their test.

--- include/metaprogramming/basic_type_traits.h
+++ include/metaprogramming/basic_type_traits.h
@@ -1,38 +1,71 @@
+#include <complex>
+
 #ifndef ERDO_BASICTYPETRAITS_H
 #define ERDO_BASICTYPETRAITS_H
 
+#define BASIC_TYPE_TRAIT(Type, StructName)                                     \
+  template <typename T> struct StructName {                                    \
+    static const bool value = false;                                           \
+  };                                                                           \
+  template <> struct StructName<Type> {                                        \
+    static const bool value = true;                                            \
+  };
+
 namespace erdo {
 
-template <typename T> struct IsVoid {
-  static const bool value = false;
-};
+// Void
+BASIC_TYPE_TRAIT(void, IsVoid)
+BASIC_TYPE_TRAIT(const void, IsConstVoid)
 
-template <> struct IsVoid<void> {
-  static const bool value = true;
-};
+// Character
+BASIC_TYPE_TRAIT(char, IsChar)
+BASIC_TYPE_TRAIT(const char, IsConstChar)
 
-// Integer
-template <typename T> struct IsInt {
-  static const bool value = false;
-};
+// Unsigned Character
+BASIC_TYPE_TRAIT(unsigned char, IsUnsignedChar)
+BASIC_TYPE_TRAIT(const unsigned char, IsConstUnsignedChar)
 
-template <> struct IsInt<int> {
-  static const bool value = true;
-};
+// Short
+BASIC_TYPE_TRAIT(short, IsShort)
+BASIC_TYPE_TRAIT(const short, IsConstShort)
 
-// Long
-template <typename T> struct IsLong {
-  static const bool value = false;
-};
+// unsigned Short
+BASIC_TYPE_TRAIT(unsigned short, IsUnsignedShort)
+BASIC_TYPE_TRAIT(const unsigned short, IsConstUnsignedShort)
 
-template <> struct IsLong<long> {
-  static const bool value = true;
-};
+// int
+BASIC_TYPE_TRAIT(int, IsInt)
+BASIC_TYPE_TRAIT(const int, IsConstInt)
 
-// IsIntegral
-template <typename T> struct IsIntegral {
-  static const bool value = IsInt<T>::value || IsLong<T>::value;
-};
+// Unsigned int
+BASIC_TYPE_TRAIT(unsigned int, IsUnsignedInt)
+BASIC_TYPE_TRAIT(const unsigned int, IsConstUnsignedInt)
 
-}
+// long
+BASIC_TYPE_TRAIT(long int, IsLong)
+BASIC_TYPE_TRAIT(const long int, IsConstLong)
+
+// Unsigned long
+BASIC_TYPE_TRAIT(unsigned long int, IsUnsignedLong)
+BASIC_TYPE_TRAIT(const unsigned long int, IsConstUnsignedLong)
+
+// long long
+BASIC_TYPE_TRAIT(long long int, IsLongLongInt)
+BASIC_TYPE_TRAIT(const long long int, IsConstLongLongInt)
+
+// Unsigned long long
+BASIC_TYPE_TRAIT(unsigned long long int, IsUnsignedLongLongInt)
+BASIC_TYPE_TRAIT(const unsigned long long int, IsConstUnsignedLongLongInt)
+
+// Double
+BASIC_TYPE_TRAIT(double, IsDouble)
+BASIC_TYPE_TRAIT(const double, IsConstDouble)
+
+BASIC_TYPE_TRAIT(long double, IsLongDouble)
+BASIC_TYPE_TRAIT(const long double, IsConstLongDouble)
+
+BASIC_TYPE_TRAIT(float, IsFloat)
+BASIC_TYPE_TRAIT(const float, IsConstFloat)
+
+} // namespace erdo
 #endif
--- tests/BasicTypeTraitsTest.cpp
+++ tests/BasicTypeTraitsTest.cpp
@@ -2,13 +2,197 @@
 #include "metaprogramming/basic_type_traits.h"
 #include <gtest/gtest.h>
 
-TEST(BasicTypeTraits, IsVoid){
+TEST(BasicTypeTraits, IsVoid) {
   constexpr bool notVoid = erdo::IsVoid<int>::value;
   constexpr bool yesVoid = erdo::IsVoid<void>::value;
-  
+
   ASSERT_FALSE(notVoid);
   ASSERT_TRUE(yesVoid);
 }
+
+TEST(BasicTypeTraits, IsConstVoid) {
+  constexpr bool notVoid = erdo::IsConstVoid<int>::value;
+  constexpr bool yesVoid = erdo::IsConstVoid<const void>::value;
+
+  ASSERT_FALSE(notVoid);
+  ASSERT_TRUE(yesVoid);
+}
+TEST(BasicTypeTraits, IsInt) {
+  constexpr bool notInt = erdo::IsInt<double>::value;
+  constexpr bool yesInt = erdo::IsInt<int>::value;
+
+  ASSERT_FALSE(notInt);
+  ASSERT_TRUE(yesInt);
+}
+
+TEST(BasicTypeTraits, IsConstInt) {
+  constexpr bool notConstInt = erdo::IsConstInt<int>::value;
+  constexpr bool yesConstInt = erdo::IsConstInt<const int>::value;
+
+  ASSERT_FALSE(notConstInt);
+  ASSERT_TRUE(yesConstInt);
+}
+
+TEST(BasicTypeTraits, IsLong) {
+  constexpr bool notLong = erdo::IsLong<int>::value;
+  constexpr bool yesLong = erdo::IsLong<long>::value;
+
+  ASSERT_FALSE(notLong);
+  ASSERT_TRUE(yesLong);
+}
+
+TEST(BasicTypeTraits, IsConstLong) {
+  constexpr bool notConstLong = erdo::IsConstLong<long>::value;
+  constexpr bool yesConstLong = erdo::IsConstLong<const long>::value;
+
+  ASSERT_FALSE(notConstLong);
+  ASSERT_TRUE(yesConstLong);
+}
+
+TEST(BasicTypeTraits, IsShort) {
+  constexpr bool notShort = erdo::IsShort<int>::value;
+  constexpr bool yesShort = erdo::IsShort<short>::value;
+
+  ASSERT_FALSE(notShort);
+  ASSERT_TRUE(yesShort);
+}
+
+TEST(BasicTypeTraits, IsConstShort) {
+  constexpr bool notConstShort = erdo::IsConstShort<short>::value;
+  constexpr bool yesConstShort = erdo::IsConstShort<const short>::value;
+
+  ASSERT_FALSE(notConstShort);
+  ASSERT_TRUE(yesConstShort);
+}
+
+TEST(BasicTypeTraits, IsChar) {
+  constexpr bool notChar = erdo::IsChar<int>::value;
+  constexpr bool yesChar = erdo::IsChar<char>::value;
+
+  ASSERT_FALSE(notChar);
+  ASSERT_TRUE(yesChar);
+}
+
+TEST(BasicTypeTraits, IsConstChar) {
+  constexpr bool notConstChar = erdo::IsConstChar<char>::value;
+  constexpr bool yesConstChar = erdo::IsConstChar<const char>::value;
+
+  ASSERT_FALSE(notConstChar);
+  ASSERT_TRUE(yesConstChar);
+}
+
+TEST(BasicTypeTraits, IsUnsignedInt) {
+  constexpr bool notUnsignedInt = erdo::IsUnsignedInt<int>::value;
+  constexpr bool yesUnsignedInt = erdo::IsUnsignedInt<unsigned int>::value;
+
+  ASSERT_FALSE(notUnsignedInt);
+  ASSERT_TRUE(yesUnsignedInt);
+}
+
+TEST(BasicTypeTraits, IsConstUnsignedInt) {
+  constexpr bool notConstUnsignedInt = erdo::IsConstUnsignedInt<int>::value;
+  constexpr bool yesConstUnsignedInt = erdo::IsConstUnsignedInt<const unsigned int>::value;
+
+  ASSERT_FALSE(notConstUnsignedInt);
+  ASSERT_TRUE(yesConstUnsignedInt);
+}
+
+TEST(BasicTypeTraits, IsUnsignedLong) {
+  constexpr bool notUnsignedLong = erdo::IsUnsignedLong<long>::value;
+  constexpr bool yesUnsignedLong = erdo::IsUnsignedLong<unsigned long>::value;
+
+  ASSERT_FALSE(notUnsignedLong);
+  ASSERT_TRUE(yesUnsignedLong);
+}
+
+TEST(BasicTypeTraits, IsConstUnsignedLong) {
+  constexpr bool notConstUnsignedLong = erdo::IsConstUnsignedLong<long>::value;
+  constexpr bool yesConstUnsignedLong = erdo::IsConstUnsignedLong<const unsigned long>::value;
+
+  ASSERT_FALSE(notConstUnsignedLong);
+  ASSERT_TRUE(yesConstUnsignedLong);
+}
+
+TEST(BasicTypeTraits, IsUnsignedShort) {
+  constexpr bool notUnsignedShort = erdo::IsUnsignedShort<short>::value;
+  constexpr bool yesUnsignedShort = erdo::IsUnsignedShort<unsigned short>::value;
+
+  ASSERT_FALSE(notUnsignedShort);
+  ASSERT_TRUE(yesUnsignedShort);
+}
+
+TEST(BasicTypeTraits, IsConstUnsignedShort) {
+  constexpr bool notConstUnsignedShort = erdo::IsConstUnsignedShort<short>::value;
+  constexpr bool yesConstUnsignedShort = erdo::IsConstUnsignedShort<const unsigned short>::value;
+
+  ASSERT_FALSE(notConstUnsignedShort);
+  ASSERT_TRUE(yesConstUnsignedShort);
+}
+
+TEST(BasicTypeTraits, IsUnsignedChar) {
+  constexpr bool notUnsignedChar = erdo::IsUnsignedChar<char>::value;
+  constexpr bool yesUnsignedChar = erdo::IsUnsignedChar<unsigned char>::value;
+
+  ASSERT_FALSE(notUnsignedChar);
+  ASSERT_TRUE(yesUnsignedChar);
+}
+
+TEST(BasicTypeTraits, IsConstUnsignedChar) {
+  constexpr bool notConstUnsignedChar = erdo::IsConstUnsignedChar<char>::value;
+  constexpr bool yesConstUnsignedChar = erdo::IsConstUnsignedChar<const unsigned char>::value;
+
+  ASSERT_FALSE(notConstUnsignedChar);
+  ASSERT_TRUE(yesConstUnsignedChar);
+}
+
+TEST(BasicTypeTraits, IsDouble) {
+  constexpr bool notDouble = erdo::IsDouble<int>::value;
+  constexpr bool yesDouble = erdo::IsDouble<double>::value;
+
+  ASSERT_FALSE(notDouble);
+  ASSERT_TRUE(yesDouble);
+}
+
+TEST(BasicTypeTraits, IsConstDouble) {
+  constexpr bool notConstDouble = erdo::IsConstDouble<int>::value;
+  constexpr bool yesConstDouble = erdo::IsConstDouble<const double>::value;
+
+  ASSERT_FALSE(notConstDouble);
+  ASSERT_TRUE(yesConstDouble);
+}
+
+TEST(BasicTypeTraits, IsFloat) {
+  constexpr bool notFloat = erdo::IsFloat<double>::value;
+  constexpr bool yesFloat = erdo::IsFloat<float>::value;
+
+  ASSERT_FALSE(notFloat);
+  ASSERT_TRUE(yesFloat);
+}
+
+TEST(BasicTypeTraits, IsConstFloat) {
+  constexpr bool notConstFloat = erdo::IsConstFloat<double>::value;
+  constexpr bool yesConstFloat = erdo::IsConstFloat<const float>::value;
+
+  ASSERT_FALSE(notConstFloat);
+  ASSERT_TRUE(yesConstFloat);
+}
+
+TEST(BasicTypeTraits, IsLongDouble) {
+  constexpr bool notLongDouble = erdo::IsLongDouble<float>::value;
+  constexpr bool yesLongDouble = erdo::IsLongDouble<long double>::value;
+
+  ASSERT_FALSE(notLongDouble);
+  ASSERT_TRUE(yesLongDouble);
+}
+
+TEST(BasicTypeTraits, IsConstLongDouble) {
+  constexpr bool notConstLongDouble = erdo::IsConstLongDouble<float>::value;
+  constexpr bool yesConstLongDouble = erdo::IsConstLongDouble<const long double>::value;
+
+  ASSERT_FALSE(notConstLongDouble);
+  ASSERT_TRUE(yesConstLongDouble);
+}
+
 
 int main(int argc, char *argv[]) {
 
